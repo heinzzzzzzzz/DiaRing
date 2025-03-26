e35b48547fcc61cfce0ca3b1837ba4e740693be8
heinzzzzzzzz <allenkalbo0707@gmail.com>
2025-03-26 13:22:15 +0800

    Create NavGraph.kt

---
+++ NavGraph.kt
@@ -0,0 +1,35 @@
+package com.example.diaring.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.diaring.ui.screens.HomeScreen
+import com.example.diaring.ui.screens.FoodPlannerScreen
+import com.example.diaring.ui.screens.ActivityRecommendationsScreen
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object FoodPlanner : Screen("food_planner")
+    object ActivityRecommendations : Screen("activity_recommendations")
+}
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+    
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.FoodPlanner.route) {
+            FoodPlannerScreen(navController)
+        }
+        composable(Screen.ActivityRecommendations.route) {
+            ActivityRecommendationsScreen(navController)
+        }
+    }
+}
