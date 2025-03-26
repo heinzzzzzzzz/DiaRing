04d04b86b48df8e91b74f2bc90eade525f69909b
heinzzzzzzzz <allenkalbo0707@gmail.com>
2025-03-26 13:15:26 +0800

    Create Color.kt

---
+++ Color.kt
@@ -0,0 +1,17 @@
+package com.example.diaring.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light theme colors
+val PrimaryLight = Color(0xFF2E7D32) // Green shade for health/wellness
+val SecondaryLight = Color(0xFF4CAF50)
+val TertiaryLight = Color(0xFF81C784)
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color.White
+
+// Dark theme colors
+val PrimaryDark = Color(0xFF81C784)
+val SecondaryDark = Color(0xFF4CAF50)
+val TertiaryDark = Color(0xFF2E7D32)
+val BackgroundDark = Color(0xFF1C1B1F)
+val SurfaceDark = Color(0xFF2C2B2F)
